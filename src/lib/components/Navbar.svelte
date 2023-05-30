eb4ceb2e5fb175d3da0c64c2eb5394c9943d77d7
Luigi Morel <morelmiles@gmail.com>
2023-05-30 03:21:01 +0300

    feat: add web page components

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,70 @@
+<script>
+	import Button from './Buttons/Button.svelte';
+</script>
+
+<nav class="bg-white border-gray-200">
+	<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+		<a href="https://flowbite.com/" class="flex items-center">
+			<img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+			<span class="self-center text-2xl font-semibold whitespace-nowrap"> Flowbite </span>
+		</a>
+		<div class="flex md:order-2">
+			<Button type="large" left href="google.com">Get Started</Button>
+
+			<button
+				data-collapse-toggle="navbar-cta"
+				type="button"
+				class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+				aria-controls="navbar-cta"
+				aria-expanded="false">
+				<span class="sr-only">Open main menu</span>
+				<svg
+					class="w-6 h-6"
+					aria-hidden="true"
+					fill="currentColor"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg">
+					<path
+						fill-rule="evenodd"
+						d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+						clip-rule="evenodd" />
+				</svg>
+			</button>
+		</div>
+
+		<div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+			<ul
+				class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
+				<li>
+					<a
+						href="/"
+						class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0"
+						aria-current="page">
+						Home
+					</a>
+				</li>
+				<li>
+					<a
+						href="/"
+						class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">
+						About
+					</a>
+				</li>
+				<li>
+					<a
+						href="/"
+						class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">
+						Services
+					</a>
+				</li>
+				<li>
+					<a
+						href="/"
+						class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">
+						Contact
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,14 @@
 <script>
-	import Header from './Header.svelte';
-	import './styles.css';
+	import Footer from '../lib/components/Footer.svelte';
+	import Hero from '../lib/components/Hero.svelte';
+	import Navbar from '../lib/components/Navbar.svelte';
+	import Services from '../lib/components/Services.svelte';
+	import './../styles/app.css';
 </script>
 
-<div class="app">
-	<Header />
-
-	<main>
-		<slot />
-	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+<div class=" bg-red-400">
+	<Navbar />
+	<Hero />
+	<Services />
+	<Footer />
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
