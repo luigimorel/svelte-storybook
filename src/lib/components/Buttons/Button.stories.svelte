f19d7bc0c99c30a51021f736f2921e80d74cabb0
Luigi Morel <morelmiles@gmail.com>
2023-05-30 03:18:45 +0300

    feat: add button component documentation

---
+++ src/lib/components/Buttons/Button.stories.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { Meta, Story } from '@storybook/addon-svelte-csf';
+
+	import Button from './Button.svelte';
+</script>
+
+<Meta title="Buttons" component={Button} />
+
+<!-- Small -->
+<Story name="Small">
+	<Button href="https://google.com">Small Button</Button>
+</Story>
+
+<!-- medium -->
+<Story name="Medium">
+	<Button type="medium" href="https://google.com">Medium Button</Button>
+</Story>
+
+<!-- large-->
+<Story name="Large">
+	<Button type="large" href="https://google.com">Large Button</Button>
+</Story>
+
+<!-- extra-large-->
+<Story name="Extra Large">
+	<Button type="extra-large" href="https://google.com">Extra Large Button</Button>
+</Story>
---
+++ src/lib/components/Buttons/Button.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	export let href: string = '';
+
+	// Icon position
+	export let left: boolean = false;
+	let iconPosition: string;
+
+	let horizontalPadding: string;
+
+	if (left) {
+		horizontalPadding = 'pl-3 pr-4';
+	} else {
+		horizontalPadding = 'pr-3 pl-4';
+	}
+
+	// Button sizes
+	/**
+	 * @type {'small' | 'medium' | 'large' | 'extra-large' }
+	 */
+
+	export let type: string = 'small';
+
+	let buttonType: string;
+
+	if (type === 'small') {
+		buttonType = 'bg-slate-500 text-xs px-5 hover:bg-slate-500/90';
+	} else if (type === 'medium') {
+		buttonType = ' bg-red-500 px-7 text-sm hover:bg-red-500/90';
+	} else if (type === 'large') {
+		buttonType = ' bg-teal-500  px-9 text-base hover:bg-teal-500/90';
+	} else {
+		buttonType = ' bg-violet-500 text-lg px-12 hover:bg-violet-500/90';
+	}
+</script>
+
+<a class={` btn ${buttonType} ${iconPosition}`} {href} target="_blank" rel="noreferrer">
+	<div class="flex items-center gap-2">
+		<slot />
+	</div>
+</a>
